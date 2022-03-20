66a10a2843def83933294f02cac743a24d271cbd
Kdaniel-prog <74417423+Kdaniel-prog@users.noreply.github.com>
2022-03-21 00:06:18 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SchoolUI'`, () => {
+  it(`should have as title 'Angular11JwtAuth'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SchoolUI');
+    expect(app.title).toEqual('Angular11JwtAuth');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SchoolUI app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular11JwtAuth app is running!');
   });
 });
